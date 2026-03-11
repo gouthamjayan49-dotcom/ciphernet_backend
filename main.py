6668d7616296020dd2be4526a1d13c1407d4d80e
Goutham Jayan <gouthamjayan49@gmail.com>
2026-03-11 21:11:33 +0530

    added database and config setup

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from core.database import database
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "CipherNet backend is alive!"}
